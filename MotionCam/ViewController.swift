72d6141670f3da1870cf603babe6bd21f3e61eb1
Hari Raja <harihraja@gmail.com>
2019-07-26 17:23:37 -0700

    Basic Layout: Images & MotionTime rows

--- MotionCam/ViewController.swift
+++ MotionCam/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var times = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        times.append(NSDate.init().description)
+        times.append(NSDate.init().description)
+        times.append(NSDate.init().description)
+        tableView.reloadData()
     }
 
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "timeCell", for: indexPath)
+        cell.textLabel?.text = times[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
 
